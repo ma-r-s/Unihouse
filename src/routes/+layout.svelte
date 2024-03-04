99274fd5459d17add769ca6b46aa9e2da194d86f
Mario Ruiz <marioalejandroruizsarmiento@gmail.com>
2024-03-03 19:10:19 -0500

    Vercel Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script>
 	import '../app.pcss';
 	import { ModeWatcher } from 'mode-watcher';
-	import { Toaster } from "$lib/components/ui/sonner";
+	import { Toaster } from '$lib/components/ui/sonner';
+	import { inject } from '@vercel/analytics';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+	inject();
+	injectSpeedInsights();
 </script>
 
 <ModeWatcher />
